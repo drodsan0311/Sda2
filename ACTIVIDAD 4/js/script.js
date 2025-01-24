a4674cc13c0460513d36e8b2495ea3a9dbd1c1b7
Daniel Rodríguez Sánchez <drodsan0311@g.educaand.es>
2025-01-24 11:57:06 +0100

    Add files via upload

---
+++ ACTIVIDAD 4/js/script.js
@@ -0,0 +1,8 @@
+function calcular() {
+ let a = parseInt(prompt("dime el primer numero"),10);
+ let b = parseInt(prompt("dime el segundo numero"),10);
+ alert ("El primer numero es:" + a + "\nEl segundo numero es: " + b + "\nLa suma es: " + (a+b) + "\nLa resta es " + (a-b) + "\nLa multiplicacion es " + (a*b) + "\nLa division es " + (a/b) + "\nEl resto es " + (a%b) +"")
+}
+
+
+
