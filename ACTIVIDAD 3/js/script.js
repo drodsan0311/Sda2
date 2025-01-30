4594be99a2db3cc0115cfbf38be82f83a7e3238f
Daniel Rodríguez Sánchez <drodsan0311@g.educaand.es>
2025-01-30 13:58:38 +0100

    Update script.js

--- ACTIVIDAD 3/js/script.js
+++ ACTIVIDAD 3/js/script.js
@@ -1,7 +1,7 @@
 function pedirDatos() {
- let nombre = prompt("¿Como te llamas?")
- let edad = prompt("¿Que edad tienes?")
- alert (" Hola " + nombre + " veo que tienes " + edad + " años ")
+ let nombre = prompt("¿Como te llamas?");
+ let edad = prompt("¿Que edad tienes?");
+ alert (" Hola " + nombre + " veo que tienes " + edad + " años ");
 }
 
 
