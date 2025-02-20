9f799b6d77dd9aa20cc1b695e9e33a4953acca29
Daniel Rodríguez Sánchez <drodsan0311@g.educaand.es>
2025-02-20 14:20:28 +0100

    Add files via upload

---
+++ ACTIVIDAD 7/js/script.js
@@ -0,0 +1,24 @@
+function calcular(){
+    let nota = parseFloat(document.getElementById("a").value, 10);
+    if (nota >= 0 && nota <= 10){
+        //Nota válida
+        document.getElementById("resultado").innerHTML = "La nota es valida";
+            if (nota >= 0 && nota < 5){
+                document.getElementById("resultado").innerHTML = "suspenso"
+            }
+            if(nota >= 5 && nota < 7){
+                document.getElementById("resultado").innerHTML="suficiente"
+            }
+            if (nota >= 7 && nota < 8){
+                document.getElementById("resultado").innerHTML = "BIEN"
+            }
+            if (nota >=10 && nota <= 8){
+                document.getElementById("resultado").innerHTML= "excelente"
+            }
+    }
+    else{
+        document.getElementById("resultado").innerHTML = "Esta nota no es valida";
+    }
+}
+
+
